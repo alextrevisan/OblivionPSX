16b3e6a8c924f1159610ed61441dba3c9bd671d1
Alexandro Trevisan <alexbaixista@gmail.com>
2026-02-05 10:17:38 -0300

    rendering optimization

--- scenario/scene_renderer.h
+++ scenario/scene_renderer.h
@@ -58,9 +58,20 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
     SVECTOR screenPts[9];
     for(auto quadIndex : model.quads)
     {
-        const CVECTOR colors[4] = {model.colors[quadIndex.color1],model.colors[quadIndex.color0], model.colors[quadIndex.color2], model.colors[quadIndex.color3]};
-        const DVECTOR uvs[4] = {model.uvs[quadIndex.uv1], model.uvs[quadIndex.uv0], model.uvs[quadIndex.uv2], model.uvs[quadIndex.uv3]};
-        gte_ldv3_f(model.vertices[quadIndex.vertice1], model.vertices[quadIndex.vertice0], model.vertices[quadIndex.vertice2]);
+        const CVECTOR* c0 = &model.colors[quadIndex.color0];
+        const CVECTOR* c1 = &model.colors[quadIndex.color1];
+        const CVECTOR* c2 = &model.colors[quadIndex.color2];
+        const CVECTOR* c3 = &model.colors[quadIndex.color3];
+        const DVECTOR* uv0 = &model.uvs[quadIndex.uv0];
+        const DVECTOR* uv1 = &model.uvs[quadIndex.uv1];
+        const DVECTOR* uv2 = &model.uvs[quadIndex.uv2];
+        const DVECTOR* uv3 = &model.uvs[quadIndex.uv3];
+        const SVECTOR* v0 = &model.vertices[quadIndex.vertice0];
+        const SVECTOR* v1 = &model.vertices[quadIndex.vertice1];
+        const SVECTOR* v2 = &model.vertices[quadIndex.vertice2];
+        const SVECTOR* v3 = &model.vertices[quadIndex.vertice3];
+        
+        gte_ldv3_f(*v1, *v0, *v2);
         gte_rtpt_b();
         gte_nclip_b();
         gte_stopz_m(p);
@@ -73,6 +84,8 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         gte_stsxy0(&screenPts[0].vx);
         gte_stsxy1(&screenPts[1].vx);
         gte_stsxy2(&screenPts[2].vx);
+        
+        int dist = p<<2;
         if (p > P_DIST_SUBDIVIDE)
         {
             setPolyGT4(next_primitive);
@@ -82,23 +95,21 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
             next_primitive->y1 = screenPts[1].vy;
             next_primitive->x2 = screenPts[2].vx;
             next_primitive->y2 = screenPts[2].vy;
-            gte_ldv0_f(model.vertices[quadIndex.vertice3]);
+            gte_ldv0_f(*v3);
             gte_rtps_b();
             gte_stsxy(&screenPts[3].vx);
             next_primitive->x3 = screenPts[3].vx;
             next_primitive->y3 = screenPts[3].vy;
-            int dist = p<<2;
-            //gte_DpqColor3(&model.colors[quadIndex.color1], &model.colors[quadIndex.color0], &model.colors[quadIndex.color2], dist, &out[0], &out[1], &out[2]);
-            gte_ldrgb3(&model.colors[quadIndex.color1], &model.colors[quadIndex.color0], &model.colors[quadIndex.color2]);
+            
             gte_lddp(dist);
+            gte_ldrgb3(c1, c0, c2);
             gte_dpct_b();
             gte_strgb3(&out[0], &out[1], &out[2]);
             setRGB0(next_primitive, out[0].r, out[0].g, out[0].b);
             setRGB1(next_primitive, out[1].r, out[1].g, out[1].b);
             setRGB2(next_primitive, out[2].r, out[2].g, out[2].b);
 
-            gte_ldrgb(&model.colors[quadIndex.color3]);
-            gte_lddp(dist);
+            gte_ldrgb(c3);
             gte_dpcs_b();
             gte_strgb(&out[3]);
             setRGB3(next_primitive, out[3].r, out[3].g, out[3].b);
@@ -106,14 +117,14 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
             next_primitive->tpage = tpage;
             next_primitive->clut = clut;
             
-            next_primitive->u0 = model.uvs[quadIndex.uv1].vx ;
-            next_primitive->v0 = model.uvs[quadIndex.uv1].vy;
-            next_primitive->u1 = model.uvs[quadIndex.uv0].vx;
-            next_primitive->v1 = model.uvs[quadIndex.uv0].vy;
-            next_primitive->u2 = model.uvs[quadIndex.uv2].vx;
-            next_primitive->v2 = model.uvs[quadIndex.uv2].vy;
-            next_primitive->u3 = model.uvs[quadIndex.uv3].vx;
-            next_primitive->v3 = model.uvs[quadIndex.uv3].vy;
+            next_primitive->u0 = uv1->vx;
+            next_primitive->v0 = uv1->vy;
+            next_primitive->u1 = uv0->vx;
+            next_primitive->v1 = uv0->vy;
+            next_primitive->u2 = uv2->vx;
+            next_primitive->v2 = uv2->vy;
+            next_primitive->u3 = uv3->vx;
+            next_primitive->v3 = uv3->vy;
             
             addPrim(orderingTable + p, next_primitive);
             next_primitive++;
@@ -130,52 +141,44 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         POLY_GT4* quad_5827 = (POLY_GT4*)next_primitive;    next_primitive += sizeof(POLY_GT4) / sizeof(*next_primitive);
         POLY_GT4* quad_8673 = (POLY_GT4*)next_primitive;    next_primitive += sizeof(POLY_GT4) / sizeof(*next_primitive);
 
-        // Interpolate positions
         gte_ldir0(2048);
-        const SVECTOR v4 = midpoint(model.vertices[quadIndex.vertice1], model.vertices[quadIndex.vertice0]);
-        const SVECTOR v5 = midpoint(model.vertices[quadIndex.vertice1], model.vertices[quadIndex.vertice2]);
-        const SVECTOR v6 = midpoint(model.vertices[quadIndex.vertice0], model.vertices[quadIndex.vertice3]);
-        const SVECTOR v7 = midpoint(model.vertices[quadIndex.vertice2], model.vertices[quadIndex.vertice3]);
+        const SVECTOR v4 = midpoint(*v1, *v0);
+        const SVECTOR v5 = midpoint(*v1, *v2);
+        const SVECTOR v6 = midpoint(*v0, *v3);
+        const SVECTOR v7 = midpoint(*v2, *v3);
         const SVECTOR v8 = midpoint(v5, v6);
 
-        //model.uvs[quadIndex.uv1], model.uvs[quadIndex.uv0], model.uvs[quadIndex.uv2], model.uvs[quadIndex.uv3]
-
-        const DVECTOR uv4 = midpoint_uv(model.uvs[quadIndex.uv1], model.uvs[quadIndex.uv0]);
-        const DVECTOR uv5 = midpoint_uv(model.uvs[quadIndex.uv1], model.uvs[quadIndex.uv2]);
-        const DVECTOR uv6 = midpoint_uv(model.uvs[quadIndex.uv0], model.uvs[quadIndex.uv3]);
-        const DVECTOR uv7 = midpoint_uv(model.uvs[quadIndex.uv2], model.uvs[quadIndex.uv3]);
+        const DVECTOR uv4 = midpoint_uv(*uv1, *uv0);
+        const DVECTOR uv5 = midpoint_uv(*uv1, *uv2);
+        const DVECTOR uv6 = midpoint_uv(*uv0, *uv3);
+        const DVECTOR uv7 = midpoint_uv(*uv2, *uv3);
         const DVECTOR uv8 = midpoint_uv(uv5, uv6);
 
-        const CVECTOR color4 = midpoint_color(model.colors[quadIndex.color1], model.colors[quadIndex.color0]);
-        const CVECTOR color5 = midpoint_color(model.colors[quadIndex.color1], model.colors[quadIndex.color2]);
-        const CVECTOR color6 = midpoint_color(model.colors[quadIndex.color0], model.colors[quadIndex.color3]);
-        const CVECTOR color7 = midpoint_color(model.colors[quadIndex.color2], model.colors[quadIndex.color3]);
+        const CVECTOR color4 = midpoint_color(*c1, *c0);
+        const CVECTOR color5 = midpoint_color(*c1, *c2);
+        const CVECTOR color6 = midpoint_color(*c0, *c3);
+        const CVECTOR color7 = midpoint_color(*c2, *c3);
         const CVECTOR color8 = midpoint_color(color5, color6);
 
-        int dist = p<<2;
         CVECTOR out_colors[9];
-        gte_ldrgb3(&model.colors[quadIndex.color1], &model.colors[quadIndex.color0], &model.colors[quadIndex.color2]);
         gte_lddp(dist);
+        gte_ldrgb3(c1, c0, c2);
         gte_dpct_b();
         gte_strgb3(&out_colors[0], &out_colors[1], &out_colors[2]);
         
-        gte_ldrgb(&model.colors[quadIndex.color3]);
-        gte_lddp(dist);
+        gte_ldrgb(c3);
         gte_dpcs_b();
         gte_strgb(&out_colors[3]);
         
         gte_ldrgb3(&color4, &color5, &color6);
-        gte_lddp(dist);
         gte_dpct_b();
         gte_strgb3(&out_colors[4], &out_colors[5], &out_colors[6]);
         
         gte_ldrgb(&color7);
-        gte_lddp(dist);
         gte_dpcs_b();
         gte_strgb(&out_colors[7]);
         
         gte_ldrgb(&color8);
-        gte_lddp(dist);
         gte_dpcs_b();
         gte_strgb(&out_colors[8]);
 
@@ -184,7 +187,7 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         gte_stsxy0(&screenPts[4].vx);
         gte_stsxy1(&screenPts[5].vx);
         gte_stsxy2(&screenPts[8].vx);
-        gte_ldv3(&model.vertices[quadIndex.vertice3], &v6, &v7);
+        gte_ldv3(v3, &v6, &v7);
         gte_rtpt();
         gte_stsxy0(&screenPts[3].vx);
         gte_stsxy1(&screenPts[6].vx);
@@ -195,23 +198,15 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         setXY4(quad_5827, screenPts[5].vx, screenPts[5].vy, screenPts[8].vx, screenPts[8].vy, screenPts[2].vx, screenPts[2].vy, screenPts[7].vx, screenPts[7].vy);
         setXY4(quad_8673, screenPts[8].vx, screenPts[8].vy, screenPts[6].vx, screenPts[6].vy, screenPts[7].vx, screenPts[7].vy, screenPts[3].vx, screenPts[3].vy);
 
-        setPolyGT4(quad_0458);
-        setPolyGT4(quad_4186);
-        setPolyGT4(quad_5827);
-        setPolyGT4(quad_8673);
+        POLY_GT4* quads[4] = {quad_0458, quad_4186, quad_5827, quad_8673};
+        for(int i = 0; i < 4; i++) {
+            setPolyGT4(quads[i]);
+            quads[i]->clut = clut;
+            quads[i]->tpage = tpage;
+        }
 
-        quad_0458->clut = clut;
-        quad_4186->clut = clut;
-        quad_5827->clut = clut;
-        quad_8673->clut = clut;
-
-        quad_0458->tpage = tpage;
-        quad_4186->tpage = tpage;
-        quad_5827->tpage = tpage;
-        quad_8673->tpage = tpage;
-
-        quad_0458->u0 = uvs[0].vx;
-        quad_0458->v0 = uvs[0].vy;
+        quad_0458->u0 = uv1->vx;
+        quad_0458->v0 = uv1->vy;
         quad_0458->u1 = uv4.vx;
         quad_0458->v1 = uv4.vy;
         quad_0458->u2 = uv5.vx;
@@ -221,8 +216,8 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
 
         quad_4186->u0 = uv4.vx;
         quad_4186->v0 = uv4.vy;
-        quad_4186->u1 = uvs[1].vx;
-        quad_4186->v1 = uvs[1].vy;
+        quad_4186->u1 = uv0->vx;
+        quad_4186->v1 = uv0->vy;
         quad_4186->u2 = uv8.vx;
         quad_4186->v2 = uv8.vy;
         quad_4186->u3 = uv6.vx;
@@ -232,8 +227,8 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         quad_5827->v0 = uv5.vy;
         quad_5827->u1 = uv8.vx;
         quad_5827->v1 = uv8.vy;
-        quad_5827->u2 = uvs[2].vx;
-        quad_5827->v2 = uvs[2].vy;
+        quad_5827->u2 = uv2->vx;
+        quad_5827->v2 = uv2->vy;
         quad_5827->u3 = uv7.vx;
         quad_5827->v3 = uv7.vy;
 
@@ -243,8 +238,8 @@ void render3DModel(Graphics* graphics, MATRIX* cameraMatrix, const T& model, TIM
         quad_8673->v1 = uv6.vy;
         quad_8673->u2 = uv7.vx;
         quad_8673->v2 = uv7.vy;
-        quad_8673->u3 = uvs[3].vx;
-        quad_8673->v3 = uvs[3].vy;
+        quad_8673->u3 = uv3->vx;
+        quad_8673->v3 = uv3->vy;
 
         setRGB0(quad_0458, out_colors[0].r, out_colors[0].g, out_colors[0].b);
         setRGB1(quad_0458, out_colors[4].r, out_colors[4].g, out_colors[4].b);
